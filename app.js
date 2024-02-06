1828648f73fe1bb2b2ea7f0aa0c4fe480d6ebfc0
Uche O. Emmanuel <ucheodilachi@gmail.com>
2024-02-07 00:11:05 +0100

    commented out all the code

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const mainBar = document.querySelector('.mainBar')
-const closeBtn = document.querySelector('.closeBtn')
-const navbar = document.querySelector('.navSection')
-const text = navbar.classList.contains('mainBar')
-const navBarFunc = ()=>{
+// const mainBar = document.querySelector('.mainBar')
+// const closeBtn = document.querySelector('.closeBtn')
+// const navbar = document.querySelector('.navSection')
+// const text = navbar.classList.contains('mainBar')
+// const navBarFunc = ()=>{
    
-        navbar.classList.add('newBar')
-mainBar.classList.add('close')
-}
+//         navbar.classList.add('newBar')
+// mainBar.classList.add('close')
+// }
 
-const closeButton = ()=>{
-     navbar.classList.remove('newBar')
-      mainBar.classList.remove('close')
-}
+// const closeButton = ()=>{
+//      navbar.classList.remove('newBar')
+//       mainBar.classList.remove('close')
+// }
 
 
 
-closeBtn.addEventListener('click', closeButton)
-mainBar.addEventListener('click', navBarFunc)
+// closeBtn.addEventListener('click', closeButton)
+// mainBar.addEventListener('click', navBarFunc)
